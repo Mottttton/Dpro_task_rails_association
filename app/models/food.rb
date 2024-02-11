9edf5fa9f05c26a4f1da779a9a5d7dbe50be1f84
Mottttton <mottttton.webdev@gmail.com>
2024-02-11 22:12:47 +0900

    modify: association between orders and foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,5 @@
 class Food < ApplicationRecord
-  has_and_belongs_to_many :orders
+  has_many :order_foods
+  has_many :orders, through: :order_foods
   belongs_to :shop
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
-  has_and_belongs_to_many :foods
+  has_many :order_foods
+  has_many :orders, through: :order_foods
   belongs_to :customer
   belongs_to :address
 end
